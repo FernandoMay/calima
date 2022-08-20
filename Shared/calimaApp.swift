1853fb199737ac9f9bfc1eacce1e5f0a1be9a401
Fernando May <fmay130@gmail.com>
2022-08-19 21:54:13 -0500

    Initial Commit

---
+++ Shared/calimaApp.swift
@@ -0,0 +1,17 @@
+//
+//  calimaApp.swift
+//  Shared
+//
+//  Created by Fernando May on 19/08/22.
+//
+
+import SwiftUI
+
+@main
+struct calimaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
